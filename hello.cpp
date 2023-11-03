a9e33df0d271cdd590be0acaccce7d0e0fde00c2
Jakub Glazik <jakubglazik02@gmail.com>
2023-11-04 00:57:50 +0100

    added cpp file

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cout<<"Ile masz lat?"<<endl;
+    cin>>a;
+    cout<<"Masz "<<a<<" lat"<<endl;
+    return 0;
+}
